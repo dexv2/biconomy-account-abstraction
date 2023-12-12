69e4be4d94a41b33a19da39903a84c4260ad25e6
phil002109 <vermontphil.paguiligan@gmail.com>
2023-12-12 18:45:31 +0800

    Add Transfer.tsx

---
+++ src/components/Transfer.tsx
@@ -0,0 +1,10 @@
+import { BiconomySmartAccount } from "@biconomy/account";
+import { useState } from "react";
+
+export default function Transfer({
+    smartAccount,
+}: {
+    smartAccount: BiconomySmartAccount
+}) {
+    const [smartContractAddress, setSmartContractAddress] = useState('')
+}
