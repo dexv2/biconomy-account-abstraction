dc9e9a69abc5403bc35470e96bc5f48fdb704343
phil002109 <vermontphil.paguiligan@gmail.com>
2023-12-13 15:49:46 +0800

    Add balanceOf to ERC20ABI

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -53,4 +53,23 @@ export const ERC20ABI = [
         stateMutability: "view",
         type: "function",
     },
+    {
+        inputs: [
+            {
+                internalType: "address",
+                name: "holder",
+                type: "address",
+            },
+        ],
+        name: "balanceOf",
+        outputs: [
+            {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
 ]
